765cab90b7f08f7cd116531439634676df26877c
Luo Yihang <c200211@e.ntu.edu.sg>
2021-11-11 16:43:24 +0800

    add footer componoent

--- client/components/Layout.js
+++ client/components/Layout.js
@@ -1,10 +1,12 @@
 import React from "react";
 import styles from "../styles/Layout.module.css";
+import Footer from "./Footer";
 
 export default function Layout({ children }) {
   return (
     <div className={styles.container}>
       <main className={styles.main}>{children}</main>
+      <Footer />
     </div>
   );
 }
