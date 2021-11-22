01a203ced2f0ad2bc5c2b72befc7b51bb09b226f
unknown <brendanawjang@gmail.com>
2021-11-23 01:55:17 +0800

    complete env setup with example file

--- server/app/config/db.config.js
+++ server/app/config/db.config.js
@@ -1,4 +1,5 @@
-require("dotenv").config();
+const path = require("path");
+require("dotenv").config({ path: `${__dirname}/.env.local` });
 
 module.exports = {
   HOST: process.env.HOST,
--- server/app/models/db.js
+++ server/app/models/db.js
@@ -1,4 +1,4 @@
-const mysql = require("mysql2/promise");
+const mysql = require("mysql2");
 const dbConfig = require("../config/db.config.js");
 
 var connection = mysql.createPool({
