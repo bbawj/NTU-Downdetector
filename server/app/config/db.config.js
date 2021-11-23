49f369da618fbdbce71d649d1b790400f1784690
unknown <brendanawjang@gmail.com>
2021-11-23 20:49:13 +0800

    delete server folder and contents

--- server/app/config/db.config.js
+++
@@ -1,9 +0,0 @@
-const path = require("path");
-require("dotenv").config({ path: `${__dirname}/.env.local` });
-
-module.exports = {
-  HOST: process.env.HOST,
-  USER: process.env.USER,
-  PASSWORD: process.env.PASSWORD,
-  DB: "ntudowndetector",
-};
