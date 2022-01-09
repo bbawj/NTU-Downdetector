e9cc2d7aae8023a882cfb140e96257e982a228a4
unknown <brendanawjang@gmail.com>
2022-01-09 14:35:21 +0800

    switch to react-bootstrap for js components

--- components/Header.js
+++ components/Header.js
@@ -1,59 +1,28 @@
 import React from "react";
 import Link from "next/link";
-import { useRouter } from "next/router";
+import Navbar from "react-bootstrap/Navbar";
+import Container from "react-bootstrap/Container";
+import Nav from "react-bootstrap/Nav";
 
 export default function Header() {
-  const router = useRouter();
-  const curPath = router.pathname;
-
   return (
-    <nav className="navbar navbar-expand-sm navbar-light bg-light w-100">
-      <div className="container-fluid">
-        <Link href="/">
-          <a className="navbar-brand">
-            NTU <span className="text-primary">Down</span>detector
-          </a>
-        </Link>
-        <button
-          className="navbar-toggler"
-          type="button"
-          data-bs-toggle="collapse"
-          data-bs-target="#navbarSupportedContent"
-          aria-controls="navbarSupportedContent"
-          aria-expanded="false"
-          aria-label="Toggle navigation"
-        >
-          <span className="navbar-toggler-icon"></span>
-        </button>
-        <div className="collapse navbar-collapse" id="navbarSupportedContent">
-          <ul className="navbar-nav ms-auto">
-            <li className="nav-item">
-              <Link href="/">
-                <a
-                  className="nav-link"
-                  {...(curPath === "/"
-                    ? { "aria-current": "page", className: "nav-link active" }
-                    : {})}
-                >
-                  Home
-                </a>
-              </Link>
-            </li>
-            <li className="nav-item">
-              <Link href="/about">
-                <a
-                  className="nav-link"
-                  {...(curPath === "/about"
-                    ? { "aria-current": "page", className: "nav-link active" }
-                    : {})}
-                >
-                  About us
-                </a>
-              </Link>
-            </li>
-          </ul>
-        </div>
-      </div>
-    </nav>
+    <Navbar bg="light" expand="sm" className="w-100">
+      <Container>
+        <Navbar.Brand href="#home">
+          <Link href="/">
+            <a className="navbar-brand">
+              NTU <span className="text-primary">Down</span>detector
+            </a>
+          </Link>
+        </Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="ms-auto">
+            <Nav.Link href="#home">Home</Nav.Link>
+            <Nav.Link href="#link">About us</Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
   );
 }
