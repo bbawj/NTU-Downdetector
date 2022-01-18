03b8b5a0911e21c67397c4eba799da6a3a6fda17
unknown <brendanawjang@gmail.com>
2022-01-18 14:00:52 +0800

    Add https to production url

--- next.config.js
+++ next.config.js
@@ -3,7 +3,7 @@ const path = require("path");
 module.exports = {
   reactStrictMode: true,
   env: {
-    productionURL: "ntu-downdetector.vercel.app",
+    productionURL: "https://ntu-downdetector.vercel.app",
   },
   sassOptions: {
     includePaths: [path.join(__dirname, "styles")],
